9b8ce2578f61921fde2d8340fa0674eb3487bf6d
Md. Kowsher <ahmedshuvo969@gamil.com>
2020-02-24 00:03:41 +0600

    Add files via upload

---
+++ Menu Adding/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Menu Adding Record',
+    'summary': """This module will add a record to store the menu name of module""",
+    'version': '1.0.0',
+    'description': """This module will add a record to the menu name of module""",
+    'author': 'NkSoft',
+    'company': 'NkSoft',
+    'website': 'https://www.nksoft.com',
+    'category': 'Tools',
+    'depends': ['base'],
+    'license': 'AGPL-3',
+    'data': [
+        'view/view.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+
+    'image': [
+        'static/description/icon.png'
+    ],
+}
+
